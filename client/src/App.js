cd6062ad70adc63f4718296315de4d4edcb2c577
OliverQian7 <71441588+OliverQian7@users.noreply.github.com>
2021-11-29 13:14:57 -0500

    run create react app

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import premlogo from './premlogo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Premier League Player Database</h1>
+        <img src={premlogo} className="logo" />
+        <p>
+          Welcome to the Premier League Player Database
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
