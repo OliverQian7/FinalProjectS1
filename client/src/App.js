5c4e77e513a368952a10e1ecc92c4bb421a172cd
OliverQian7 <71441588+OliverQian7@users.noreply.github.com>
2021-11-30 14:12:32 -0500

    created player component and added like button

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,33 @@
 import premlogo from './premlogo.png';
+import son from './son.png';
+import salah from './salah.png';
 import './App.css';
+import React, {useState} from 'react'
+import Player from './Player'
 
 function App() {
+
+  const [players, setPlayers] = useState([
+    {
+      pic: son,
+      title: "Heung-min Son",
+      club: "Spurs",
+      country: "South Korea",
+      appearances: "209",
+      goals: "74",
+      assists: "40"
+    },
+    {
+      pic: salah,
+      title: "Mohamed Salah",
+      club: "Liverpool",
+      country: "Egypt",
+      appearances: "171",
+      goals: "108",
+      assists: "42"
+    }
+  ])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,15 +36,80 @@ function App() {
         <p>
           Welcome to the Premier League Player Database
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> 
-        <h1>player</h1>
+        <h2>players</h2>
+        <div><input id = "search"></input><input type="submit" value="Search"></input></div>
+        <br></br>
+        <table>
+        <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Club</th>
+            <th>Country</th>
+            <th>Appearances</th>
+            <th>Goals</th>
+            <th>Assists</th>
+            <th>Likes</th>
+          </tr>
+          {players.map((player) => 
+          <Player 
+            pic = {player.pic}
+            title = {player.title}
+            club = {player.club}
+            country = {player.country}
+            appearances = {player.appearances}
+            goals = {player.goals}
+            assists = {player.assists}
+          />
+          )}
+          
+        </table>
+        <h2>teams</h2>
+        <table>
+          <tr>
+          <th></th>
+          <th></th>
+          <th>W</th>
+          <th>D</th>
+          <th>L</th>
+          <th>GF</th>
+          <th>GA</th>
+          <th>GD</th>
+          <th>P</th>
+          </tr>
+          <tr>
+            <th>1</th>
+            <th>Chelsea</th>
+            <th>9</th>
+            <th>3</th>
+            <th>1</th>
+            <th>31</th>
+            <th>5</th>
+            <th>+26</th>
+            <th>30</th>
+          </tr>
+          <tr>
+            <th>2</th>
+            <th>Manchester City</th>
+            <th>9</th>
+            <th>2</th>
+            <th>2</th>
+            <th>27</th>
+            <th>7</th>
+            <th>+20</th>
+            <th>29</th>
+          </tr>
+          <tr>
+            <th>3</th>
+            <th>Liverpool</th>
+            <th>8</th>
+            <th>4</th>
+            <th>1</th>
+            <th>39</th>
+            <th>11</th>
+            <th>+28</th>
+            <th>28</th>
+          </tr>
+        </table>
       </header>
     </div>
   );
---
+++ client/src/Player.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react'
+
+function Player({pic, title, club, country, appearances, goals, assists, likeText}) {
+
+  const[likes, setlikes] = useState(0)
+
+    return (
+        <tr>
+        <th id = "playerpic"><img src = {pic} id = "playerpiture" /></th>
+        <th>{title}</th>
+        <th>{club}</th>
+        <th>{country}</th>
+        <th>{appearances}</th>
+        <th>{goals}</th>
+        <th>{assists}</th>
+        <th>{likeText} {likes}    <input type = "submit" value = "Like" onClick = {() => {
+          setlikes(likes+1)
+        }} /></th>
+      </tr>
+    )
+}
+
+export default Player
